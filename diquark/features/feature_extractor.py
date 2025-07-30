0e7ee4f09a38579a79d6aea114875c21b668d5d8
Gabriel Majeri <gabriel.majeri6@gmail.com>
2025-07-30 20:33:22 +0300

    Implement new feature extractor

--- diquark/features/feature_extractor.py
+++ diquark/features/feature_extractor.py
@@ -9,6 +9,145 @@ class FeatureExtractor:
         self.feature_names = self._generate_feature_names()
 
     def _generate_feature_names(self) -> list[str]:
+        statistics = ("min", "mean", "max")
+        features = [
+            "jet_multiplicity",
+            "combined_invariant_mass",
+            "sphericity",
+            "aplanarity",
+            "centrality",
+            "total_energy",
+            "total_transverse_momentum",
+            *(f"chi2_{i}_{s}" for s in statistics for i in range(1, 4)),
+            "2jet_invariant_mass_mean",
+            "2jet_invariant_mass_stddev",
+            "3jet_invariant_mass_mean",
+            "3jet_invariant_mass_stddev",
+            "6jet_invariant_mass_mean",
+            "6jet_invariant_mass_stddev",
+        ]
+
+        return features
+
+    def _pad_jets_array(self, array: ak.Array):
+        padded = ak.pad_none(array, self.n_jets, clip=True)
+        filled = ak.fill_none(padded, 0.0)
+        return filled.to_numpy()
+
+    def _n_jet_invariant_mass(self, energy, p_x, p_y, p_z, n):
+        combination_indices = ak.unzip(ak.argcombinations(energy, n=n))
+        return np.sqrt(
+            sum(energy[combination_indices[i]] for i in range(n)) ** 2 -
+            sum(p_x[combination_indices[i]] for i in range(n)) ** 2 -
+            sum(p_y[combination_indices[i]] for i in range(n)) ** 2 -
+            sum(p_z[combination_indices[i]] for i in range(n)) ** 2
+        )
+
+    def compute_all(self, data: ak.Array) -> dict[str, np.ndarray]:
+        num_jets = data['Jet'].to_numpy()
+
+        # Extract jet 4-vector components
+        p_T = data['Jet/Jet.PT']
+        eta = data['Jet/Jet.Eta']
+        phi = data['Jet/Jet.Phi']
+
+        # Compute components in Cartesian coordinates
+        p_x = p_T * np.cos(phi)
+        p_y = p_T * np.sin(phi)
+        p_z = p_T * np.sinh(eta)
+
+        # Compute jet energy
+        energy = p_T * np.cosh(eta)
+
+        p_x_np = self._pad_jets_array(p_x)
+        p_y_np = self._pad_jets_array(p_y)
+        p_z_np = self._pad_jets_array(p_z)
+        energy_np = self._pad_jets_array(energy)
+
+        momenta = np.moveaxis(np.stack((p_x_np, p_y_np, p_z_np)), 0, -1)
+        total = (momenta * momenta).sum(axis=-1).sum(axis=-1)
+
+        non_zero_jets = num_jets > 0
+
+        S = np.zeros((len(data[non_zero_jets]), 3, 3))
+        for i in range(3):
+            for j in range(3):
+                S[:, i, j] = np.vecdot(momenta[non_zero_jets, :, i], momenta[non_zero_jets, :, j]) / total[non_zero_jets]
+
+        eigenvalues = np.linalg.eigvalsh(S)
+
+        eigenvalues = np.sort(eigenvalues, axis=-1)
+        lambda_2 = eigenvalues[:, 1]
+        lambda_3 = eigenvalues[:, 0]
+
+        sphericity = np.full_like(num_jets, np.nan, dtype=np.float64)
+        aplanarity = np.full_like(num_jets, np.nan, dtype=np.float64)
+
+        sphericity[non_zero_jets] = 3/2 * (lambda_2 + lambda_3)
+        aplanarity[non_zero_jets] = 3/2 * lambda_3
+
+        total_energy = ak.sum(energy, axis=-1).to_numpy()
+        H_T = ak.sum(p_T, axis=-1).to_numpy()
+        centrality = H_T / total_energy
+
+        combined_invariant_mass = np.sqrt(
+            energy_np.sum(axis=-1) ** 2
+            - p_x_np.sum(axis=-1) ** 2
+            - p_y_np.sum(axis=-1) ** 2
+            - p_z_np.sum(axis=-1) ** 2
+        )
+
+        m2j = self._n_jet_invariant_mass(energy, p_x, p_y, p_z, n=2)
+        m3j = self._n_jet_invariant_mass(energy, p_x, p_y, p_z, n=3)
+        m6j = self._n_jet_invariant_mass(energy, p_x, p_y, p_z, n=6)
+
+        m_W = 80.3692
+        sigma_W = 20
+
+        m_chi = 2000
+        sigma_chi = 2/100 * m_chi
+
+        m_S = 8000
+        sigma_S = 100
+
+        chi2_first_component = ((m2j - m_W) / sigma_W) ** 2
+        chi2_second_component = ((m3j - m_chi) / sigma_chi) ** 2
+        chi2_third_component = ((m6j - m_S) / sigma_S) ** 2
+
+        features = {
+            "jet_multiplicity": num_jets,
+            "combined_invariant_mass": combined_invariant_mass,
+            "sphericity": sphericity,
+            "aplanarity": aplanarity,
+            "centrality": centrality,
+            "total_energy": total_energy,
+            "total_transverse_momentum": H_T,
+            "chi2_1_min": ak.min(chi2_first_component, axis=-1).to_numpy(),
+            "chi2_1_mean": ak.mean(chi2_first_component, axis=-1).to_numpy(),
+            "chi2_1_max": ak.max(chi2_first_component, axis=-1).to_numpy(),
+            "chi2_2_min": ak.min(chi2_second_component, axis=-1).to_numpy(),
+            "chi2_2_mean": ak.mean(chi2_second_component, axis=-1).to_numpy(),
+            "chi2_2_max": ak.max(chi2_second_component, axis=-1).to_numpy(),
+            "chi2_3_min": ak.min(chi2_third_component, axis=-1).to_numpy(),
+            "chi2_3_mean": ak.mean(chi2_third_component, axis=-1).to_numpy(),
+            "chi2_3_max": ak.max(chi2_third_component, axis=-1).to_numpy(),
+            "m2j_mean": ak.nanmean(m2j, axis=-1).to_numpy(),
+            "m2j_std": ak.nanstd(m2j, axis=-1).to_numpy(),
+            "m3j_mean": ak.nanmean(m3j, axis=-1).to_numpy(),
+            "m3j_std": ak.nanstd(m3j, axis=-1).to_numpy(),
+            "m6j_mean": ak.nanmean(m6j, axis=-1).to_numpy(),
+            "m6j_std": ak.nanstd(m6j, axis=-1).to_numpy(),
+        }
+
+        return features
+
+
+class OldFeatureExtractor:
+    def __init__(self, n_jets: int):
+        self.n_jets = n_jets
+        self.feature_names = self._generate_feature_names()
+
+    def _generate_feature_names(self) -> list[str]:
         basic_features = [
             "jet_multiplicity",
             *[f"leading_jet_pt_{i+1}" for i in range(self.n_jets)],
